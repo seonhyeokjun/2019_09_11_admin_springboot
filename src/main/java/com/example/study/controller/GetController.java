d8709ca5e7116bfc0a355768e9e1611a31896e0f
선혁준 <tjsrmatn@gmail.com>
2019-09-10 19:58:10 +0900

    add test

--- src/main/java/com/example/study/controller/GetController.java
+++ src/main/java/com/example/study/controller/GetController.java
@@ -1,9 +1,6 @@
 package com.example.study.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api") // locathost:8080/api
@@ -15,7 +12,10 @@ public class GetController {
     }
 
     @GetMapping("/getParameter") //localhost:8080/api/getParamerter?id=1234&password=abcd
-    public String getParameter(){
+    public String getParameter(@RequestParam String id, @RequestParam String password){
+        System.out.println("id = "+id);
+        System.out.println("password ="+password);
 
+        return id+password;
     }
 }
