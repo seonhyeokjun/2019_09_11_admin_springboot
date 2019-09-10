215a1a25bc94f9f117fd7ea293c4cd3c9ee4964e
선혁준 <tjsrmatn@gmail.com>
2019-09-10 19:51:36 +0900

    add test

--- src/main/java/com/example/study/controller/GetController.java
+++ src/main/java/com/example/study/controller/GetController.java
@@ -1,5 +1,6 @@
 package com.example.study.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,10 @@ public class GetController {
     @RequestMapping(method =  RequestMethod.GET, path = "/getMethod") // localhost:8080/api/getMethod
     public String getRequest(){
         return "Hi getMethod";
+    }
+
+    @GetMapping("/getParameter") //localhost:8080/api/getParamerter?id=1234&password=abcd
+    public String getParameter(){
+
     }
 }
