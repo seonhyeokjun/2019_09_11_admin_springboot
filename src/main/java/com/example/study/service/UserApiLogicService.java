7400a58711efc7c2c71d019e7dc3d6343a5b676b
선혁준 <tjsrmatn@gmail.com>
2019-09-21 19:39:13 +0900

    사용자 API UPDATE - version(1)

--- src/main/java/com/example/study/service/UserApiLogicService.java
+++ src/main/java/com/example/study/service/UserApiLogicService.java
@@ -55,7 +55,28 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
 
     @Override
     public Header<UserApiResponse> update(Header<UserApiRequest> request) {
-        return null;
+        // 1. data
+        UserApiRequest userApiRequest = request.getData();
+
+        // 2. id -> user 데이터를 찾고
+        Optional<User> optional = userRepository.findById(userApiRequest.getId());
+
+        return optional.map(user -> {
+            // 3. data -> update
+            // id
+            user.setAccount(userApiRequest.getAccount())
+                    .setPassword(userApiRequest.getPassword())
+                    .setStatus(userApiRequest.getStatus())
+                    .setPhoneNumber(userApiRequest.getPhoneNumber())
+                    .setEmail(userApiRequest.getEmail())
+                    .setRegisteredAt(userApiRequest.getRegisteredAt())
+                    .setUnregisteredAt(userApiRequest.getUnregisteredAt());
+
+            return user;
+        })
+        .map(user -> userRepository.save(user)) // update -> newUser
+        .map(updateUser -> response(updateUser)) // userApiResponse
+        .orElseGet(() -> Header.ERROR("데이터 없음"));
     }
 
     @Override
