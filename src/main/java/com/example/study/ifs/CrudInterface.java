7369f01d1e87e9febc4119e654193365d74d8d1b
선혁준 <tjsrmatn@gmail.com>
2019-09-21 15:23:09 +0900

    사용자 API create - version(2)

--- src/main/java/com/example/study/ifs/CrudInterface.java
+++ src/main/java/com/example/study/ifs/CrudInterface.java
@@ -6,11 +6,11 @@ import com.example.study.model.network.response.UserApiResponse;
 
 public interface CrudInterface<Req,Res> {
 
-    Header<Res> create(Req request);  // todo request objcet 추가
+    Header<Res> create(Header<Req> request);  // todo request objcet 추가
 
     Header<Res> read(Long id);
 
-    Header<Res> update(Req request);
+    Header<Res> update(Header<Req> request);
 
     Header delete(Long id);
 }
--- src/main/java/com/example/study/service/UserApiLogicService.java
+++ src/main/java/com/example/study/service/UserApiLogicService.java
@@ -1,6 +1,7 @@
 package com.example.study.service;
 
 import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.User;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.UserApiRequest;
 import com.example.study.model.network.response.UserApiResponse;
@@ -8,18 +9,33 @@ import com.example.study.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class UserApiLogicService implements CrudInterface<UserApiRequest, UserApiResponse> {
 
     @Autowired
     private UserRepository userRepository;
 
-    // 1. request data
-    // 2. user 생성
-    // 3. 생선된 데이터 -> UserApiResponse return
     @Override
-    public Header<UserApiResponse> create(UserApiRequest request) {
-        return null;
+    public Header<UserApiResponse> create(Header<UserApiRequest> request) {
+
+        // 1. request data
+        UserApiRequest userApiRequest = request.getData();
+
+        // 2. User 생성
+        User user = User.builder()
+                .account(userApiRequest.getAccount())
+                .password(userApiRequest.getPassword())
+                .status("REGISTERED")
+                .phoneNumber(userApiRequest.getPhoneNumber())
+                .email(userApiRequest.getEmail())
+                .registeredAt(LocalDateTime.now())
+                .build();
+        User newUser = userRepository.save(user);
+
+        // 3. 생선된 데이터 -> UserApiResponse return
+        return response(newUser);
     }
 
     @Override
@@ -28,12 +44,30 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
     }
 
     @Override
-    public Header<UserApiResponse> update(UserApiRequest request) {
+    public Header<UserApiResponse> update(Header<UserApiRequest> request) {
         return null;
     }
 
     @Override
     public Header delete(Long id) {
         return null;
+    }
+
+    private Header<UserApiResponse> response(User user){
+        // user -> userApiResponse
+
+        UserApiResponse userApiResponse = UserApiResponse.builder()
+                .id(user.getId())
+                .account(user.getAccount())  // todo 암호화, 길이
+                .password(user.getPassword())
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .status(user.getStatus())
+                .registeredAt(user.getRegisteredAt())
+                .unregisteredAt(user.getUnregisteredAt())
+                .build();
+
+        // Header + data return
+        return Header.OK(userApiResponse);
     }
 }
